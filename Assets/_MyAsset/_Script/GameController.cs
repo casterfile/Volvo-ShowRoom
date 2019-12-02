50886c1adde307add54f9384dd5547b0447952b5
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-02 10:20:15 +0800

    Add all my files

---
+++ Assets/_MyAsset/_Script/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject Popup01;
+	// Use this for initialization
+	void Start () {
+        Popup01.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowPopup()
+    {
+        Popup01.SetActive(false);
+    }
+}
---
+++ Assets/_MyAsset/_Script/TouchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TouchController : MonoBehaviour {
+    public GameObject GObject;
+    public void OnMouseDown()
+    {
+        GObject.SetActive(true);
+    }
+}
